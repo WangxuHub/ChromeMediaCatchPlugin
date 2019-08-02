f85e4435f795f6c8257be64a4d63ffbf2a5143e6
shuangfanguang <695166116@qq.com>
2019-08-03 01:28:08 +0800

    图标 更新

---
+++ src/background.js
@@ -0,0 +1,15 @@
+
+
+// function log() {
+//     console.log(new Date().getTime());
+// }
+
+// setInterval(function () {
+//     log();
+// }, 500);
+
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.storage.sync.set({ color: '#3aa757' }, function () {
+        console.log("The color is green.");
+    });
+});
