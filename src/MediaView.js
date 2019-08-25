f27a271eb9e7e671f9a9537bd5e81244bb93dffa
shuangfanguang <695166116@qq.com>
2019-08-25 23:23:29 +0800

    Canvas 获取video 内容完成

--- src/MediaView.js
+++ src/MediaView.js
@@ -1,23 +1,21 @@
 
 
   var $video = $("#main-video");
-  $video.css('min-height', $video.width() / 16 * 9 + 'px');
+  //$video.css('min-height', $video.width() / 16 * 9 + 'px');
 
  var player = videojs('main-video', {
      controls: true,
-     //sources: [{
-     //    src: '/video/v1.mp4',
-     //    type: 'video/mp4'
-     //}]
+     sources: [{
+        src: '1.mp4',
+        type: 'video/mp4'
+     }]
      //  language:'zh-CN'
  });
 
  window.player = player;
  
- var link = './assets/123.m3u8';
- // var link = 'https://pl-ali.youku.com/playlist/m3u8?vid=XMjY4NjM5Mzc0MA%3D%3D&type=mp4hdv3&ups_client_netip=3a6593ee&utid=XKrmERQ5tDkCAbeeXGydsjsZ&ccode=0502&psid=401996804baacdaa0bc46ea449ac620a&ups_userid=442966237&ups_ytid=442966237&duration=1433&expire=18000&drm_type=1&drm_device=7&play_ability=1024&ups_ts=1558884113&onOff=0&encr=0&ups_key=be98c1d23ca26bb96b595a40c850dfab';
- // var link = 'https://valipl-vip.cp31.ott.cibntv.net/6572A8805983B717F3AC8508D/03000600005C6B7F1B0F653011BA6AB0B7511C-FD5A-49DD-A115-B35EF7C8B8C4-1-114.m3u8?ccode=0502&duration=1433&expire=18000&psid=a2430f38c13e458db0a5ae2c54d0765c&ups_client_netip=3a6593ee&ups_ts=1558882963&ups_userid=442966237&utid=jphxFY%2FkBiQCATplk%2B67zVyD&vid=XMjY4NjM5Mzc0MA%3D%3D&vkey=A1f5e0afbef081630dcb56f7069baeebb&sm=1&operate_type=1&bc=2';
- player.src({ src: link, type: "application/x-mpegURL" });
+//  var link = './assets/123.m3u8';
+//  player.src({ src: link, type: "application/x-mpegURL" });
 
 
 
@@ -29,24 +27,47 @@
 
 
 
-var methodHandler = {};
-var msgType = "view"
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    var cmdType = request.cmdType;
-    var argus = request.argus||[];
-    argus.push(sendResponse);
+var video = $video[0];
+var canvas = document.getElementById("canvasVideo");
+var context = canvas.getContext("2d");
 
-    if (cmdType.indexOf(msgType + ".") !== 0) {
-        return;
-    }
+$("#btnPlay").click(function () {
+  canvas.width = $video[0].videoWidth;
+  canvas.height = $video[0].videoHeight;
 
-    var innerCmdType = cmdType.substr(msgType.length+1);
-
-    methodHandler[innerCmdType].apply(methodHandler, argus);
+  var timer = null;
+  function draw1() {//绘制视频
+    video.play();
+    timer = setInterval(function () {
+      context.drawImage(video, 0, 0, canvas.width, canvas.height);//绘制视频
+    }, 1000/30);
+  };
+  draw1();
 });
 
 
-methodHandler.setSrc = function (src, cb) {
-  src += "12312312";
-  cb(src);
-}
+
+
+
+// // 
+// var methodHandler = {};
+// var msgType = "view"
+// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+//     var cmdType = request.cmdType;
+//     var argus = request.argus||[];
+//     argus.push(sendResponse);
+
+//     if (cmdType.indexOf(msgType + ".") !== 0) {
+//         return;
+//     }
+
+//     var innerCmdType = cmdType.substr(msgType.length+1);
+
+//     methodHandler[innerCmdType].apply(methodHandler, argus);
+// });
+
+
+// methodHandler.setSrc = function (src, cb) {
+//   src += "12312312";
+//   cb(src);
+// }
